735e47489788243bb08c10330eb25db64fa341ff
Simple code editor <bijaysingha226@gmail.com>
2025-08-14 20:01:10 +0530

    Add files via upload

--- Delta-builds/alpine.sh
+++ Delta-builds/alpine.sh
@@ -1,60 +1,98 @@
-dist_name="Alpine Linux"
-dist_version="3.22.1"
+#!/data/data/com.termux/files/usr/bin/bash
 
-bootstrap_distribution() {
-	sudo rm -f "${ROOTFS_DIR}"/alpine-*.tar.xz
+# Install required packages
+apt install proot proot-distro bsdtar axel neofetch -y
+clear
 
-	for arch in aarch64 armv7 riscv64 x86 x86_64; do
-		curl --fail --location \
-			--output "${WORKDIR}/alpine-minirootfs-${dist_version}-${arch}.tar.gz" \
-			"https://dl-cdn.alpinelinux.org/alpine/v${dist_version:0:4}/releases/${arch}/alpine-minirootfs-${dist_version}-${arch}.tar.gz"
-		curl --fail --location \
-			--output "${WORKDIR}/alpine-minirootfs-${dist_version}-${arch}.tar.gz.sha256" \
-			"https://dl-cdn.alpinelinux.org/alpine/v${dist_version:0:4}/releases/${arch}/alpine-minirootfs-${dist_version}-${arch}.tar.gz.sha256"
-		sha256sum -c "${WORKDIR}/alpine-minirootfs-${dist_version}-${arch}.tar.gz.sha256"
+# Read FS value
+FS=alpine
 
-		sudo rm -rf "${WORKDIR}/alpine-$(translate_arch "$arch")"
-		sudo mkdir -m 755 "${WORKDIR}/alpine-$(translate_arch "$arch")"
-		sudo tar -zxp --acls --xattrs --xattrs-include='*' \
-			-f "${WORKDIR}/alpine-minirootfs-${dist_version}-${arch}.tar.gz" \
-			-C "${WORKDIR}/alpine-$(translate_arch "$arch")"
+# Forward the value of FS to NM
+NM=$FS
 
-		cat <<- EOF | sudo unshare -mpf bash -e -
-		rm -f "${WORKDIR}/alpine-$(translate_arch "$arch")/etc/resolv.conf"
-		echo "nameserver 1.1.1.1" > "${WORKDIR}/alpine-$(translate_arch "$arch")/etc/resolv.conf"
-		mount --bind /dev "${WORKDIR}/alpine-$(translate_arch "$arch")/dev"
-		mount --bind /proc "${WORKDIR}/alpine-$(translate_arch "$arch")/proc"
-		mount --bind /sys "${WORKDIR}/alpine-$(translate_arch "$arch")/sys"
-		chroot "${WORKDIR}/alpine-$(translate_arch "$arch")" apk upgrade
-		chroot "${WORKDIR}/alpine-$(translate_arch "$arch")" apk add shadow-login
-		chroot "${WORKDIR}/alpine-$(translate_arch "$arch")" apk add apk-tools=2.14.9-r2
-		chroot "${WORKDIR}/alpine-$(translate_arch "$arch")" ln -sf /var/cache/apk /etc/apk/cache
-		EOF
+# Capitalize the first letter of NM
+NM=$(echo $NM | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
 
-		sudo rm -f "${WORKDIR:?}/alpine-$(translate_arch "$arch")"/var/cache/apk/* || true
+# Display neofetch with custom ASCII art
+neofetch --ascii_distro $NM
 
-		archive_rootfs "${ROOTFS_DIR}/alpine-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar.xz" \
-			"alpine-$(translate_arch "$arch")"
-	done
-}
+# Output the selected value
+echo "You selected $NM Linux"
+echo " "
 
-write_plugin() {
-	cat <<- EOF > "${PLUGIN_DIR}/alpine.sh"
-	# This is a default distribution plug-in.
-	# Do not modify this file as your changes will be overwritten on next update.
-	# If you want customize installation, please make a copy.
-	DISTRO_NAME="Alpine Linux"
-	DISTRO_COMMENT="Regular release v${dist_version}."
+# Create necessary directories
+mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs
 
-	TARBALL_URL['aarch64']="${GIT_RELEASE_URL}/alpine-aarch64-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['aarch64']="$(sha256sum "${ROOTFS_DIR}/alpine-aarch64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['arm']="${GIT_RELEASE_URL}/alpine-arm-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['arm']="$(sha256sum "${ROOTFS_DIR}/alpine-arm-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['i686']="${GIT_RELEASE_URL}/alpine-i686-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['i686']="$(sha256sum "${ROOTFS_DIR}/alpine-i686-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['riscv64']="${GIT_RELEASE_URL}/alpine-riscv64-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['riscv64']="$(sha256sum "${ROOTFS_DIR}/alpine-riscv64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['x86_64']="${GIT_RELEASE_URL}/alpine-x86_64-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['x86_64']="$(sha256sum "${ROOTFS_DIR}/alpine-x86_64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	EOF
-}
+# Change directory
+cd $PREFIX/var/lib/proot-distro/installed-rootfs/
+
+# Download the rootfs using axel
+axel -a -o $FS.tar.xz https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/$FS.tar.xz
+
+# Check file integrity
+echo " "
+echo "Checking $NM Linux File Integrity...!!!"
+echo " "
+sleep 3s
+echo "MD5"
+md5sum $FS.tar.xz
+sleep 3s
+echo " "
+echo "SHA256"
+sha256sum $FS.tar.xz
+sleep 3s
+echo " "
+echo "SHA512"
+sha512sum $FS.tar.xz
+sleep 3s
+
+# Extract the rootfs
+echo " "
+echo "[*] Extracting ${NM} Linux Rootfs, Please wait...!!!"
+proot --link2symlink bsdtar -xpJf $FS.tar.xz 2>/dev/null
+
+# Create a proot-distro configuration file for the Kali minimal rootfs
+cat > $PREFIX/etc/proot-distro/$FS.sh << EOF
+DISTRO_NAME="$NM Linux"
+TARBALL_URL['aarch64']="https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/$FS.tar.xz"
+TARBALL_SHA256['aarch64']="$(sha256sum $FS.tar.xz | awk '{print $1}')"
+EOF
+
+# Create a shortcut command to launch the distro
+cat > $PREFIX/bin/$FS << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro login $FS
+EOF
+chmod 700 $PREFIX/bin/$FS
+
+# Add start-up login notification
+cat >> $PREFIX/etc/bash.bashrc << EOF
+echo "Login $NM Linux: $FS"
+EOF
+
+# Add support for uninstallation
+cat > $PREFIX/bin/uninstall-$FS << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro remove $FS
+sed -i 's/echo "Login $NM Linux: $FS"//g' $PREFIX/etc/bash.bashrc
+rm -rf $PREFIX/bin/uninstall-$FS
+rm -rf $PREFIX/bin/$FS
+rm -rf $PREFIX/var/lib/proot-distro/dlcache/$FS.tar.xz
+EOF
+chmod 700 $PREFIX/bin/uninstall-$FS
+
+# Backup tarball to the cache
+mkdir -p $PREFIX/var/lib/proot-distro/dlcache
+mv $FS.tar.xz $PREFIX/var/lib/proot-distro/dlcache
+
+# Display final instructions
+echo " "
+echo -e '\e[1;96mSuccessful Installation!'
+echo -e '\e[0m'
+cat <<- EOF
+To login $NM Linux, Type: $FS
+EOF
+sleep 15s
+echo " "
