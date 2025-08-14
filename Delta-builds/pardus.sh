feba0bfbbd23bb9ec3db922174af00c41112c7ac
Simple code editor <bijaysingha226@gmail.com>
2025-08-14 20:29:11 +0530

    Add files via upload

--- Delta-builds/pardus.sh
+++ Delta-builds/pardus.sh
@@ -1,45 +1,98 @@
-dist_name="Pardus"
-dist_version="yirmiuc"
+#!/data/data/com.termux/files/usr/bin/bash
 
-bootstrap_distribution() {
-	sudo rm -f "${ROOTFS_DIR}"/pardus-*.tar.xz
+# Install required packages
+apt install proot proot-distro bsdtar axel neofetch -y
+clear
 
-	curl -LO https://depo.pardus.org.tr/pardus/pool/main/p/pardus-archive-keyring/pardus-archive-keyring_2021.1_all.deb
-	sudo dpkg -i pardus-archive-keyring_2021.1_all.deb
-	rm pardus-archive-keyring_2021.1_all.deb
+# Read FS value
+FS=pardus
 
-	for arch in arm64 i386 amd64; do
-		sudo rm -rf "${WORKDIR}/pardus-$(translate_arch "$arch")"
-		sudo mmdebstrap \
-			--architectures=${arch} \
-			--variant=minbase \
-			--components="main,contrib,non-free" \
-			--include="ca-certificates,libsystemd0,pardus-archive-keyring,systemd-sysv" \
-			--format=directory \
-			"${dist_version}-deb" \
-			"${WORKDIR}/pardus-$(translate_arch "$arch")" \
-			"deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free non-free-firmware" \
-			"deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free non-free-firmware" \
-			"deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free non-free-firmware"
-		archive_rootfs "${ROOTFS_DIR}/pardus-$(translate_arch "$arch")-pd-${CURRENT_VERSION}.tar.xz" \
-			"pardus-$(translate_arch "$arch")"
-	done
-	unset arch
-}
+# Forward the value of FS to NM
+NM=$FS
 
-write_plugin() {
-	cat <<- EOF > "${PLUGIN_DIR}/pardus.sh"
-	# This is a default distribution plug-in.
-	# Do not modify this file as your changes will be overwritten on next update.
-	# If you want customize installation, please make a copy.
-	DISTRO_NAME="Pardus"
-	DISTRO_COMMENT="Version '${dist_version}'."
+# Capitalize the first letter of NM
+NM=$(echo $NM | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
 
-	TARBALL_URL['aarch64']="${GIT_RELEASE_URL}/pardus-aarch64-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['aarch64']="$(sha256sum "${ROOTFS_DIR}/pardus-aarch64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['i686']="${GIT_RELEASE_URL}/pardus-i686-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['i686']="$(sha256sum "${ROOTFS_DIR}/pardus-i686-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	TARBALL_URL['x86_64']="${GIT_RELEASE_URL}/pardus-x86_64-pd-${CURRENT_VERSION}.tar.xz"
-	TARBALL_SHA256['x86_64']="$(sha256sum "${ROOTFS_DIR}/pardus-x86_64-pd-${CURRENT_VERSION}.tar.xz" | awk '{ print $1}')"
-	EOF
-}
+# Display neofetch with custom ASCII art
+neofetch --ascii_distro $NM
+
+# Output the selected value
+echo "You selected $NM Linux"
+echo " "
+
+# Create necessary directories
+mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs
+
+# Change directory
+cd $PREFIX/var/lib/proot-distro/installed-rootfs/
+
+# Download the rootfs using axel
+axel -a -o $FS.tar.xz https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/$FS.tar.xz
+
+# Check file integrity
+echo " "
+echo "Checking $NM Linux File Integrity...!!!"
+echo " "
+sleep 3s
+echo "MD5"
+md5sum $FS.tar.xz
+sleep 3s
+echo " "
+echo "SHA256"
+sha256sum $FS.tar.xz
+sleep 3s
+echo " "
+echo "SHA512"
+sha512sum $FS.tar.xz
+sleep 3s
+
+# Extract the rootfs
+echo " "
+echo "[*] Extracting ${NM} Linux Rootfs, Please wait...!!!"
+proot --link2symlink bsdtar -xpJf $FS.tar.xz 2>/dev/null
+
+# Create a proot-distro configuration file for the Kali minimal rootfs
+cat > $PREFIX/etc/proot-distro/$FS.sh << EOF
+DISTRO_NAME="$NM Linux"
+TARBALL_URL['aarch64']="https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/$FS.tar.xz"
+TARBALL_SHA256['aarch64']="$(sha256sum $FS.tar.xz | awk '{print $1}')"
+EOF
+
+# Create a shortcut command to launch the distro
+cat > $PREFIX/bin/$FS << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro login $FS
+EOF
+chmod 700 $PREFIX/bin/$FS
+
+# Add start-up login notification
+cat >> $PREFIX/etc/bash.bashrc << EOF
+echo "Login $NM Linux: $FS"
+EOF
+
+# Add support for uninstallation
+cat > $PREFIX/bin/uninstall-$FS << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro remove $FS
+sed -i 's/echo "Login $NM Linux: $FS"//g' $PREFIX/etc/bash.bashrc
+rm -rf $PREFIX/bin/uninstall-$FS
+rm -rf $PREFIX/bin/$FS
+rm -rf $PREFIX/var/lib/proot-distro/dlcache/$FS.tar.xz
+EOF
+chmod 700 $PREFIX/bin/uninstall-$FS
+
+# Backup tarball to the cache
+mkdir -p $PREFIX/var/lib/proot-distro/dlcache
+mv $FS.tar.xz $PREFIX/var/lib/proot-distro/dlcache
+
+# Display final instructions
+echo " "
+echo -e '\e[1;96mSuccessful Installation!'
+echo -e '\e[0m'
+cat <<- EOF
+To login $NM Linux, Type: $FS
+EOF
+sleep 15s
+echo " "
